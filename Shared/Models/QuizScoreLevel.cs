5fe280b3307ca66602e1cd547e798713afd219d4
Edvard <e.smoliakovas@gmail.com>
2024-10-15 14:16:36 +0300

    Added enum for defining quiz score

---
+++ Shared/Models/QuizScoreLevel.cs
@@ -0,0 +1,10 @@
+namespace Project.Enums
+{
+    public enum QuizScoreLevel
+    {
+        High,     // score > 80
+        Moderate, // between 60 and 79
+        Average,  // between 40 and 59
+        Low       // score < 40
+    }
+}
