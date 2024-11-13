8f2502b91013b7c7f26a1cb261df02fd21e7d62b
vik-sim <152206267+vik-sim@users.noreply.github.com>
2024-11-13 10:57:24 +0200

    added database and migrations

---
+++ Server/Migrations/20241112190038_Init.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Attempts",
+                columns: table => new
+                {
+                    UserName = table.Column<string>(type: "text", nullable: false),
+                    ReadingTime = table.Column<int>(type: "integer", nullable: false),
+                    Wpm = table.Column<int>(type: "integer", nullable: false),
+                    ParagraphId = table.Column<int>(type: "integer", nullable: false),
+                    Score = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attempts", x => x.UserName);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Paragraphs",
+                columns: table => new
+                {
+                    ParagraphId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ParagraphText = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paragraphs", x => x.ParagraphId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuizQuestions",
+                columns: table => new
+                {
+                    ParagraphId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuizQuestions", x => x.ParagraphId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Question",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    text = table.Column<string>(type: "text", nullable: false),
+                    options = table.Column<List<string>>(type: "text[]", nullable: false),
+                    correctAnswer = table.Column<string>(type: "text", nullable: false),
+                    userAnswer = table.Column<string>(type: "text", nullable: false),
+                    QuizQuestionsParagraphId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Question", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Question_QuizQuestions_QuizQuestionsParagraphId",
+                        column: x => x.QuizQuestionsParagraphId,
+                        principalTable: "QuizQuestions",
+                        principalColumn: "ParagraphId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Question_QuizQuestionsParagraphId",
+                table: "Question",
+                column: "QuizQuestionsParagraphId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attempts");
+
+            migrationBuilder.DropTable(
+                name: "Paragraphs");
+
+            migrationBuilder.DropTable(
+                name: "Question");
+
+            migrationBuilder.DropTable(
+                name: "QuizQuestions");
+        }
+    }
+}
